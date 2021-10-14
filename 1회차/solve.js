65a1b0d48d00c71cc57793e7761315bf1348380d
johyune <sharpjo4@naver.com>
2021-10-14 13:24:51 +0900

    2회차

--- 1회차/solve.js
+++ 1회차/solve.js
@@ -1,12 +1,14 @@
 function solve() {
-    const person = 100
-    const camp = ['가위', '바위', '보']
+    var result = 0
+    const person = 5
 
-    const max = Math.floor(person / camp.length) + 1
-
-    
-    
-    
+    for(가위 = 0; 가위 <= person; 가위++) {
+        for(바위 = 0; 바위 + 가위 <= person; 바위++) {
+            보 = person - 가위 - 바위
+            if([가위, 바위, 보].filter(n => n == Math.max(가위, 바위, 보)).length === 1) result++
+        }
+    }
+    console.log(`경우의 수 ${result}`)
 }
 
 solve()
---
+++ 2회차/solve.js
@@ -0,0 +1,6 @@
+function solve(start, end) {
+    var cls = (start - end) * 1 - 1
+    console.log(cls)
+}
+
+solve(1,5)
