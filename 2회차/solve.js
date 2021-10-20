6d7c4ec92d5ab64238afc6bfb1f917191e1a3141
hyunej <sharpjo4@naver.com>
2021-10-20 21:48:56 +0900

    2회차 알고리즘 설명 풀이

--- 2회차/solve.js
+++ 2회차/solve.js
@@ -2,14 +2,22 @@ function solve(start, end) {
     const total_platform = 43
     var cls = Math.abs(start - end)
     var in_result = cls - 1
-    var out_reslut = total_platform - cls - 1
+    var out_reslut = total_platform - in_result - 2
     
-    var result = ((2 ** in_result) + (2 ** out_reslut)) - 1
+    var result = (2 ** in_result) + (2 ** out_reslut) - 1
     console.log(result)
 }
 
-function official_solve() {
-     var a = 2
+//solve(1, 17)
+
+function solve2(start, end) {
+    const total_platform = 43
+    var cls = Math.abs(start - end)
+    var in_result = cls - 1
+    var out_reslut = total_platform - in_result - 2
+    
+    var result = (1 << in_result) + (1 << out_reslut) - 1
+    console.log(result)
 }
 
-solve(1, 2)
+solve2(1, 17)
