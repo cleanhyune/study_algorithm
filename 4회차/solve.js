0caf188f561115c27dc446c8218471ebc09d8d96
hyunej <sharpjo4@naver.com>
2021-10-29 14:43:20 +0900

    소수점 제거 함수 Math.floor은 반내림이 아닌 반올림으로 적용되어있었음. 하여 오류 parseInt로 수정

--- 4회차/solve.js
+++ 4회차/solve.js
@@ -14,12 +14,12 @@ function solve(length) {
         [8, 7],
         [9, 6]
     ]) /* 기본 숫자별 각 불이 들어오는 개수 */
-
+    
     const countSegmentByTime = (hour, min, sec) => {
         return [hour, min, sec].map(n => {
             var count = defaultSegments.get(n)
             if(count === undefined) {
-                const tens = defaultSegments.get(Math.round(n / 10, 0))  // 10의 자리 숫자의 불 개수
+                const tens = defaultSegments.get(parseInt(n / 10))  // 10의 자리 숫자의 불 개수
                 const units = defaultSegments.get(n % 10)                //  1의 자리 숫자의 불 개수
                 count = tens + units
                 defaultSegments.set(n, count)                            // 없던 숫자의 세그먼트 개수를 저장함으로써 중복 계산을 방지
@@ -32,7 +32,8 @@ function solve(length) {
     for(hour=0; hour<24; hour++) {
         for(min=0; min<60; min++) {
             for(sec=0; sec<60; sec++) {
-                if(countSegmentByTime(hour, min ,sec) === length) {
+                const na = countSegmentByTime(hour, min ,sec)
+                if(na === length) {
                     resultTime.push(`${regexTime(hour)}시 ${regexTime(min)}분 ${regexTime(sec)}초`)
                     result++ 
                 }
